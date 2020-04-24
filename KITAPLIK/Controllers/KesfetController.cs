9f79ffe8f98197f696fa000fe6a73447dd11e692
ASUS <ASUS@DESKTOP-QV3A2PN>
2020-04-24 17:24:38 +0300

    Keşfet Sayfası eklendi ve bir kaç fotoğraf.(salihanın değişiklikleri)

---
+++ KITAPLIK/Controllers/KesfetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KITAPLIK.Controllers
+{
+    public class KesfetController : Controller
+    {
+        // GET: Kesfet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
