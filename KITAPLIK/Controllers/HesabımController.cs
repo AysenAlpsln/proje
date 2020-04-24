e4f902a8b213a4c1991b35260412cf7caed7c5f5
ASUS <ASUS@DESKTOP-QV3A2PN>
2020-04-24 18:31:44 +0300

    anasayfada değişiklikler yapıldı ve profil sayfası oluşturukdu.

---
+++ KITAPLIK/Controllers/HesabımController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KITAPLIK.Controllers
+{
+    public class HesabımController : Controller
+    {
+        // GET: Hesabım
+        public ActionResult Hesabım()
+        {
+            return View();
+        }
+    }
+}
