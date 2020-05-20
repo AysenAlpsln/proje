4ab4b9952594c40fc991a0e004580cff66c0ea32
ASUS <ASUS@DESKTOP-QV3A2PN>
2020-05-21 02:12:51 +0300

    Hesap ayarları sayfası yapıldı fakat çalışmıyor.(tekrar bakılacak)

---
+++ KITAPLIK/Controllers/AyarlarController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KITAPLIK.Models.Entity;
+
+namespace KITAPLIK.Controllers
+{
+    public class AyarlarController : Controller
+    {
+        data3 db = new data3();
+        // GET: Ayarlar
+        [HttpPost]
+        public ActionResult Ayarlar(Uyeler k)
+        {
+            var uye = db.Uyeler.Find(k.takmaad);
+            uye.AdSoyad = k.AdSoyad;
+            uye.Cınsıyet = k.Cınsıyet;
+            uye.DogumYeri = k.DogumYeri;
+            uye.DogumTarıhı = k.DogumTarıhı;
+            uye.Telefon = k.Telefon;
+            uye.Meslegi = k.Meslegi;
+            uye.YasadıgıSehir = k.YasadıgıSehir;
+            uye.Biografi = k.Biografi;
+            db.SaveChanges();
+            return RedirectToAction("Hesabım", "Hesabım");
+        }
+    }
+}
