68eff2dd00a3a8d4b577bcd277e3d52d86f21581
ASUS <ASUS@DESKTOP-QV3A2PN>
2020-05-23 22:17:01 +0300

    Eksik kitap bildiri sayfası düzenlendi.(salihanın yaptıkları)

---
+++ KITAPLIK/Controllers/IletisimController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KITAPLIK.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // GET: İletisim
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
