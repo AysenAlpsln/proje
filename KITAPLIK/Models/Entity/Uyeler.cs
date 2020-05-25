f2ffa2664c44325ff1fefcb58f62c28aad429776
ASUS <ASUS@DESKTOP-QV3A2PN>
2020-05-25 18:21:07 +0300

    giriş yap ve kaydol türkçeleştirildi.

--- KITAPLIK/Models/Entity/Uyeler.cs
+++ KITAPLIK/Models/Entity/Uyeler.cs
@@ -11,7 +11,8 @@ namespace KITAPLIK.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Uyeler
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
